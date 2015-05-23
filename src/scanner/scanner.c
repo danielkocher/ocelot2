6f245f8f098efa6c9c9e2dcafec83670921c8ea6
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-05-23 22:30:39 +0200

    Add first (dummy) implementation of functions to make it compile

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -1,1 +1,16 @@
 #include "scanner.h"
+
+void scanner_init() {
+	scanner_token_init();
+
+	current_symbol = T_EOF;
+	scanner_get_symbol();
+}
+
+void scanner_token_init() {
+	T_EOF = -1;
+}
+
+void scanner_get_symbol() {
+	
+}
