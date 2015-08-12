833b5c0e2d1a749f2eadf78ebc110283ec2d53bf
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-08-12 23:03:22 +0200

    Add USAGE message

--- src/ocelot2.c
+++ src/ocelot2.c
@@ -1,6 +1,11 @@
 #include "scanner/scanner.h"
 
-void main() {
+void main(int argc, char** argv) {
+	if(argc <= 2) {
+        printf("USAGE: ./ozelot2 input-file output-file\n");
+        return -1;
+    }
+
 	scanner_init();
 	
 	while(current_symbol != T_EOF) {
