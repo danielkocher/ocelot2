f8f313eb36aa06001dbeb94c03612a5d373d0a1f
Daniel Kocher <dkocher@cosy.sbg.ac.at>
2017-01-29 11:47:37 +0100

    Add enable/disable functions

--- src/logger/logger.c
+++ src/logger/logger.c
@@ -6,12 +6,12 @@ void logger_init(struct logger_t* logger, int enable) {
   logger->enable = enable;
 }
 
-void logger_disable(struct eagle_t* eagle) {
-  eagle->enable = 0;
+void logger_disable(struct logger_t* logger) {
+  logger->enable = 0;
 }
 
-void logger_enable(struct eagle_t* eagle) {
-  eagle->enable = 1;
+void logger_enable(struct logger_t* logger) {
+  logger->enable = 1;
 }
 
 void logger_print(struct logger_t* logger, char* message) {
